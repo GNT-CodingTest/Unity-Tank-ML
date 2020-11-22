e79ecc2e3690acfad6c5b209184eaf2a4bbc7574
i_jemin <i_jemin@icloud.com>
2020-11-22 14:45:44 +0900

    TankHealth.cs - Complete 네임스페이스 제거

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -2,99 +2,96 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Complete
+public class TankHealth : MonoBehaviour
 {
-    public class TankHealth : MonoBehaviour
+    public float maxHealth = 100f;
+    public Slider healthSlider;
+    public Image fillImage;
+    public Color fullHealthColor = Color.green;
+    public Color zeroHealthColor = Color.red;
+
+    public GameObject tankRender;
+    private Collider _collider;
+
+
+    public GameObject explosionPrefab;
+
+    public bool IsDead { get; private set; }
+
+    private AudioSource _explosionAudio;
+    private ParticleSystem _explosionParticles;
+    public float CurrentHealth { get; private set; }
+    public event Action OnTankDead = () => { };
+
+    public void RestoreHealth(float health)
     {
-        public float maxHealth = 100f;
-        public Slider healthSlider;
-        public Image fillImage;
-        public Color fullHealthColor = Color.green;
-        public Color zeroHealthColor = Color.red;
+        CurrentHealth = Mathf.Min(maxHealth, CurrentHealth + health);
+        SetHealthUI();
+    }
 
-        public GameObject tankRender;
-        private Collider _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
 
-        
-        public GameObject explosionPrefab;
+        _explosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
+        _explosionAudio = _explosionParticles.GetComponent<AudioSource>();
+        _explosionParticles.gameObject.SetActive(false);
+    }
 
-        public bool IsDead { get; private set; }
-        
-        private AudioSource _explosionAudio;
-        private ParticleSystem _explosionParticles;
-        public float CurrentHealth { get; private set; }
-        public event Action OnTankDead = () => { };
-        
-        public void RestoreHealth(float health)
+    private void OnEnable()
+    {
+        CurrentHealth = maxHealth;
+        IsDead = false;
+        tankRender.SetActive(true);
+        _collider.enabled = true;
+        SetHealthUI();
+        healthSlider.gameObject.SetActive(true);
+    }
+
+
+    public float TakeDamage(float damage)
+    {
+        if (IsDead)
         {
-            CurrentHealth = Mathf.Min(maxHealth, CurrentHealth + health);
-            SetHealthUI();
+            return 0f;
         }
 
-        private void Awake()
+        var appliedDamage = damage;
+        if (damage >= CurrentHealth)
         {
-            _collider = GetComponent<Collider>();
-
-            _explosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
-            _explosionAudio = _explosionParticles.GetComponent<AudioSource>();
-            _explosionParticles.gameObject.SetActive(false);
+            appliedDamage = CurrentHealth;
+            Death();
+        }
+        else
+        {
+            CurrentHealth -= appliedDamage;
         }
 
-        private void OnEnable()
-        {
-            CurrentHealth = maxHealth;
-            IsDead = false;
-            tankRender.SetActive(true);
-            _collider.enabled = true;
-            SetHealthUI();
-            healthSlider.gameObject.SetActive(true);
-        }
-        
-
-        public float TakeDamage(float damage)
-        {
-            if (IsDead)
-            {
-                return 0f;
-            }
-
-            var appliedDamage = damage;
-            if (damage >= CurrentHealth)
-            {
-                appliedDamage = CurrentHealth;
-                Death();
-            }
-            else
-            {
-                CurrentHealth -= appliedDamage;
-            }
-            
-            SetHealthUI();
-            return appliedDamage;
-        }
+        SetHealthUI();
+        return appliedDamage;
+    }
 
 
-        private void SetHealthUI()
-        {
-            healthSlider.value = CurrentHealth;
-            fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, CurrentHealth / maxHealth);
-        }
+    private void SetHealthUI()
+    {
+        healthSlider.value = CurrentHealth;
+        fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, CurrentHealth / maxHealth);
+    }
 
-        public void Death()
-        {
-            IsDead = true;
-            tankRender.SetActive(false);
-            _collider.enabled = false;
-            
-            healthSlider.gameObject.SetActive(false);
+    public void Death()
+    {
+        IsDead = true;
+        tankRender.SetActive(false);
+        _collider.enabled = false;
 
-            _explosionParticles.transform.position = transform.position;
-            _explosionParticles.gameObject.SetActive(true);
-            
-            _explosionParticles.Play();
-            _explosionAudio.Play();
-            
-            OnTankDead();
-        }
+        healthSlider.gameObject.SetActive(false);
+
+        _explosionParticles.transform.position = transform.position;
+        _explosionParticles.gameObject.SetActive(true);
+
+        _explosionParticles.Play();
+        _explosionAudio.Play();
+
+        OnTankDead();
     }
 }
