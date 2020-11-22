d10bf98bd77874a99d27e20a991b88998e717b37
i_jemin <i_jemin@icloud.com>
2020-11-22 14:01:14 +0900

    InfoUI - 주석제거

--- Assets/Scripts/InfoUI.cs
+++ Assets/Scripts/InfoUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Text;
 using Unity.MLAgents;
 using UnityEngine;
@@ -10,13 +8,12 @@ public class InfoUI : MonoBehaviour
     private Agent[] _agents;
 
     public Text text;
-    // Start is called before the first frame update
+
     private void Start()
     {
         _agents = FindObjectsOfType<Agent>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         var stringBuilder = new StringBuilder();
