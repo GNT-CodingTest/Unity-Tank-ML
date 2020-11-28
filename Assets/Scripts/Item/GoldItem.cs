ffd5a4c3564cd4148db38b5064326d99bea522e1
i_jemin <i_jemin@icloud.com>
2020-11-28 16:14:32 +0900

    GoldItem reward를 필드로 뺌

--- Assets/Scripts/Item/GoldItem.cs
+++ Assets/Scripts/Item/GoldItem.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class GoldItem : Item
 {
+    public float reward = 0.2f;
     public override void Use(GameObject target)
     {
         var agent = target.GetComponent<Agent>();
 
         if (agent != null)
         {
-            agent.AddReward(0.2f);
+            agent.AddReward(reward);
             Destroy(gameObject);
         }
     }
